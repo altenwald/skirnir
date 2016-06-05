1418e8fbfdd5be975f54cb7da044c441f376142e
Manuel Rubio <manuel@altenwald.com>
2016-06-05 23:14:33 +0100

    add logger plugins for file and syslog and add postgrex for MDA storage (WIP)

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,29 @@
 use Mix.Config
 
 config :logger,
-    backends: [:console]
+    backends: [
+        :console,
+        {LoggerFileBackend, :file},
+        {Logger.Backends.Syslog, :syslog}
+    ]
 
 config :logger, :console,
     level: :debug,
     format: "$time $metadata[$level] $levelpad$message\n",
     metadata: [:pid]
+
+config :logger, :file,
+    level: :info,
+    format: "$date $time $metadata[$level] $levelpad$message\n",
+    metadata: [:pid],
+    path: "log/skirnir.log"
+
+# config :logger, :syslog,
+#     level: :info,
+#     facility: :mail,
+#     appid: "skirnir",
+#     host: "127.0.0.1",
+#     port: 514
 
 config :skirnir,
     domain: "altenwald.com",
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Skirnir.Mixfile do
      elixir: "~> 1.3.0-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     test_coverage: [tool: CoberturaCover]]
   end
 
   def application do
@@ -21,6 +22,11 @@ defmodule Skirnir.Mixfile do
      {:hashids, "~> 2.0"},
      {:timex, "~> 2.1.4"},
      {:cuttlefish, override: true, github: "basho/cuttlefish", tag: "2.0.6"},
-     {:eleveldb, github: "basho/eleveldb", tag: "2.1.0"}]
+     {:eleveldb, github: "basho/eleveldb", tag: "2.1.0"},
+     {:logger_file_backend, "~> 0.0.7"},
+     {:syslog, github: "altenwald/syslog"},
+     {:postgrex, ">= 0.0.0"},
+     # test deps:
+     {:cobertura_cover, "~> 0.9.0", only: :test}]
   end
 end
