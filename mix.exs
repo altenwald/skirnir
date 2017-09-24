2db0da09d3b49f3258ef456408c93859f05587ca
Manuel Rubio <manuel@altenwald.com>
2017-09-24 20:46:04 +0200

    update dependencies

--- mix.exs
+++ mix.exs
@@ -22,14 +22,14 @@ defmodule Skirnir.Mixfile do
   end
 
   defp deps do
-    [{:ranch, "~> 1.2.0"},
+    [{:ranch, "~> 1.4.0"},
      {:hashids, "~> 2.0"},
      {:timex, "~> 3.1.24"},
      {:logger_file_backend, "~> 0.0.7"},
      {:syslog, github: "altenwald/syslog"},
      {:postgrex, "~> 1.0.0-rc.1"},
      {:erocksdb, github: "leo-project/erocksdb", tag: "4.13.5", manager: :rebar},
-     {:json, "~> 0.3.0"},
+     {:json, "~> 1.0.2"},
      {:poolboy, "~> 1.5.0"},
      # test deps:
      {:excoveralls, "~> 0.7.3", only: :test},
