57c908695f0f14f5812f7470a853c57fbf208deb
Manuel Rubio <manuel@altenwald.com>
2018-08-17 15:53:09 +0200

    add alias for SMTP (test)

--- test/skirnir_smtp_server_test.exs
+++ test/skirnir_smtp_server_test.exs
@@ -1,12 +1,13 @@
 defmodule SkirnirSmtpServerTest do
   use ExUnit.Case
+  alias :gen_smtp_client, as: SMTP
 
   test "send an email" do
     body = 'Subject: testing\r\nFrom: Andrew Thompson \r\n' ++
            'To: Some Dude\r\n\r\nThis is the email body'
     message = {'alice@altenwald.com', ['bob@altenwald.com'], body}
     opts = [relay: 'localhost', port: 2525]
-    "2.0.0 Ok: queued as " <> _id = :gen_smtp_client.send_blocking(message, opts)
+    "2.0.0 Ok: queued as " <> _id = SMTP.send_blocking(message, opts)
 
     # clean the queue for the message sent
     assert nil != Skirnir.Smtp.Server.Queue.dequeue()
